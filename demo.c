603820afbbcf8ef5c14be12bb8c2f54f606cefc6
unknown <skynetlua@foxmail.com>
2020-04-14 09:39:37 +0800

    修复cmake文件

--- demo.c
+++ demo.c
@@ -22,27 +22,27 @@ static void handle_data(uintptr_t uid, int fd, char* data, int size)
     int session_id = (int)uid;
     if (client_session_id == session_id)
     {
-        printf("[boy]接收到服务端消息 fd = %d, data = %s \n", fd, data);
+        printf("[boy]receive server msg fd = %d, data = %s \n", fd, data);
         if (data[0] == 2)
         {
-            char msg[128] = "\3那你要出来吃饭吗？";
+            char msg[128] = "\3have a dinner?";
             mr_socket_send(fd, msg, sizeof(msg));
         }
     }
     else if (accept_session_id == session_id)
     {
-        printf("[girl]接收到客户端消息 fd = %d, data = %s \n", fd, data);
+        printf("[girl]receive client msg fd = %d, data = %s \n", fd, data);
         if (data[0] == 1)
         {
-            char msg[128] = "\2还没呢，你真贴心。^_^";
+            char msg[128] = "\2final,thank you^_^";
             mr_socket_send(fd, msg, sizeof(msg));
         }
         else if (data[0] == 3)
         {
-            char msg[128] = "\4讨厌。才刚认识呢！我们先聊聊嘛";
+            char msg[128] = "\4I don't know you";
             mr_socket_send(fd, msg, sizeof(msg));
             mr_socket_close((uintptr_t)session_id, fd);
-            printf("[girl]拒绝邀约，关闭客户端\n");
+            printf("[girl]refuse,close client\n");
         }
     }
     else
@@ -57,19 +57,19 @@ static void handle_connect(uintptr_t uid, int fd, char* data, int size)
     if (server_session_id == session_id)
     {
         assert(server_fd == fd);
-        printf("[server]服务器监听启动成功 data =%s, size = %d \n", data, size);
+        printf("[server]server listen success. data =%s, size = %d \n", data, size);
     }
     else if (client_session_id == session_id)
     {
         assert(client_fd == fd);
-        printf("\n[boy]客户端连接服务器启动成功 data =%s, size = %d \n", data, size);
-        char msg[128] = "\1靓女，你吃饭了吗？";
+        printf("\n[boy]client connect server success. data =%s, size = %d \n", data, size);
+        char msg[128] = "\1girl,how are you?";
         mr_socket_send(fd, msg, sizeof(msg));
-        printf("[boy]开始第%d次邀请妹纸\n", ++invite_count);
+        printf("[boy]invite girl %d times\n", ++invite_count);
     }
     else if (accept_session_id == session_id)
     {
-        printf("[girl]客户端连接服务器成功 data =%s, size = %d \n", data, size);
+        printf("[girl]client connect server success data =%s, size = %d \n", data, size);
     }
     else
     {
@@ -82,27 +82,27 @@ static void handle_close(uintptr_t uid, int fd, char* data, int size)
     int session_id = (int)uid;
     if (client_session_id == session_id)
     {
-        printf("[boy]连接关闭 fd = %d, data=%s, size = %d \n", fd, data, size);
+        printf("[boy]connect close fd = %d, data=%s, size = %d \n", fd, data, size);
         //等3秒，再预约
         client_fd = 0;
         timer_count = 3000;
     }
     else if (accept_session_id == session_id)
     {
-        printf("[girl]连接关闭 fd = %d, data=%s, size = %d \n", fd, data, size);
+        printf("[girl]connect close fd = %d, data=%s, size = %d \n", fd, data, size);
     }
 }
 
 static void handle_error(uintptr_t uid, int fd, char* data, int size)
 {
     int session_id = (int)uid;
-    printf("发生错误 session_id = %d, fd = %d, data=%s, size = %d \n", session_id, fd, data, size);
+    printf("error session_id = %d, fd = %d, data=%s, size = %d \n", session_id, fd, data, size);
 }
 
 static void handle_warning(uintptr_t uid, int fd, char* data, int size)
 {
     int session_id = (int)uid;
-    printf("发生警告 session_id = %d, fd = %d, data=%s, size = %d \n", session_id, fd, data, size);
+    printf("warning session_id = %d, fd = %d, data=%s, size = %d \n", session_id, fd, data, size);
 }
 
 void handle_accept(uintptr_t uid, int fd, char* data, int size, int apt_fd)
@@ -113,11 +113,11 @@ void handle_accept(uintptr_t uid, int fd, char* data, int size, int apt_fd)
     char* addr = malloc(size+1);
     memset((void*)addr, 0, sizeof(addr));
     memcpy(addr, data, size);
-    printf("[server]监听到新的链接addr=%s, apt_fd=%d \n", addr, apt_fd);
+    printf("[server]listen new connect addr=%s, apt_fd=%d \n", addr, apt_fd);
     free(addr);
     
     mr_socket_start((uintptr_t)accept_session_id, apt_fd);
-    printf("[girl]接受与客户端连接 apt_fd =%d\n", apt_fd);
+    printf("[girl]accept client apt_fd =%d\n", apt_fd);
 }
 
 int main(int argc, char* argv[])
@@ -140,7 +140,7 @@ int main(int argc, char* argv[])
 
     //启动mushroom，并启动线程监听和接收数据包
     mr_socket_run();
-    printf("[mushroom]mushroom启动成功\n");
+    printf("[mushroom]mushroom start success\n");
     //启动服务器监听
     server_fd = mr_socket_listen((uintptr_t)server_session_id, "0.0.0.0", 8080, 64);
     if (server_fd < 0) {
@@ -148,15 +148,15 @@ int main(int argc, char* argv[])
         assert(0);
     }
     mr_socket_start((uintptr_t)server_session_id, server_fd);
-    printf("[server]启动监听0.0.0.0:8080 server_fd = %d\n", server_fd);
+    printf("[server]listen:0.0.0.0:8080 server_fd = %d\n", server_fd);
     //启动连接服务器
     client_fd = mr_socket_connect((uintptr_t)client_session_id, "127.0.0.1", 8080);
     if (client_fd < 0)
     {
-        printf("[boy]连接失败 faild client_fd = %d\n", client_fd);
+        printf("[boy]connect faild client_fd = %d\n", client_fd);
         assert(0);
     }
-    printf("[boy]启动连接服务器 client_fd = %d\n", client_fd);
+    printf("[boy]connect server client_fd = %d\n", client_fd);
     while(1)
     {
         //业务线程循环检测是否有网络消息，有网络消息，就会通过回调函数返回
--- mrsocket/mr_mem.c
+++ mrsocket/mr_mem.c
@@ -6,17 +6,18 @@
 #include <assert.h>
 #include <stdbool.h>
 #if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#include "win/winport.h"
-#include "win/atomic.h"
+	#include "win/winport.h"
+	#include "win/atomic.h"
+	#include "win/spinlock.h"
 #else
-#include "atomic.h"
+	#include "atomic.h"
+	#include "spinlock.h"
 #endif
 
 #define MEMORY_ALLOCTAG 0x20190429
 #define MEMORY_FREETAG 0x0badf00d
 
 // #define MEMORY_CHECK
-
 struct mem_cookie {
 	uint32_t uid;
 	uint32_t dogtag;
@@ -32,25 +33,55 @@ static uint32_t _max_uid = 0;
 static uint32_t* uid_mems = NULL;
 static uint32_t* check_mems = NULL;
 
-inline static void* fill_prefix(char* ptr, size_t size) {
+static struct mr_mem_info* _head = NULL;
+static struct mr_mem_info* _tail = NULL;
+static struct spinlock* info_lock = NULL;
+
+
+void push_mem_info(struct mr_mem_info* info){
+	if(info_lock == NULL){
+		info_lock = (struct spinlock*)malloc(sizeof(struct spinlock));
+		spinlock_init(info_lock);
+	}
+	spinlock_lock(info_lock);
+	info->next = NULL;
+	if (_head == NULL){
+		_head = info;
+		_tail = info;
+	}else{
+		_tail->next = info;
+		_tail = info;
+	}
+	spinlock_unlock(info_lock);
+}
+
+struct mr_mem_info* mr_mem_check_info(void){
+	if(info_lock == NULL){
+		return NULL;
+	}
+	struct mr_mem_info* info;
+	spinlock_lock(info_lock);
+	info = _head;
+	_head = NULL;
+	_tail = NULL;
+	spinlock_unlock(info_lock);
+	return info;
+}
+
+static void* fill_prefix(char* ptr, size_t size) {
 	struct mem_cookie *p = (struct mem_cookie *)(ptr + size - sizeof(struct mem_cookie));
 	uint32_t uid = ATOM_INC(&_uid);
 	memcpy(&p->uid, &uid, sizeof(uid));
 	if (uid < _max_uid){
 		uid_mems[uid] = (uint32_t)size;
 		if (check_mems[uid]){
-			printf("fill_prefix check_mems uid=%d\n", uid);
+			struct mr_mem_info* info = (struct mr_mem_info*)malloc(sizeof(struct mr_mem_info));
+		 	info->uid = uid;
+		 	info->mem = uid_mems[uid];
+		 	info->next = NULL;
+		 	push_mem_info(info);
 		}
 	}
-	// if (size >= 1024){
-	// 	printf("fill_prefix uid =%d, size=%dkb %db\n", uid, size >> 10, size%1024);
-	// }else{
-	// 	printf("fill_prefix uid =%d, size=%db\n", uid, size);
-	// }
-	// if (size >= 1024*3){
-	// 	printf("fill_prefix big mem uid =%d, size=%dkb %db\n", uid, size >> 10, size%1024);
-	// }
-
 	uint32_t dogtag = MEMORY_ALLOCTAG;
 	memcpy(&p->dogtag, &dogtag, sizeof(dogtag));
 
@@ -67,10 +98,14 @@ inline static void* clean_prefix(char* ptr) {
 	uint32_t uid;
 	memcpy(&uid, &p->uid, sizeof(uid));
 	if (uid < _max_uid){
-		uid_mems[uid] = 0;
 		if (check_mems[uid]) {
-			printf("clean_prefix check_mems uid=%d\n", uid);
+			struct mr_mem_info* info = (struct mr_mem_info*)malloc(sizeof(struct mr_mem_info));
+		 	info->uid = uid;
+		 	info->mem = uid_mems[uid];
+		 	info->next = NULL;
+		 	push_mem_info(info);
 		}
+		uid_mems[uid] = 0;
 	}
 	uint32_t dogtag;
 	memcpy(&dogtag, &p->dogtag, sizeof(dogtag));
@@ -105,16 +140,38 @@ void mr_mem_free(void *ptr){
 	free(rawptr);
 }
 
-void mr_mem_info(void){
+struct mr_mem_info* mr_mem_info(void){
 	if (_max_uid > 0){
+		size_t size = 0;
+		struct mr_mem_info* head = NULL;
+		struct mr_mem_info* tail = NULL;
 		uint32_t uid = 0;
-		 for (; uid < _max_uid; ++uid){
+		for (; uid < _max_uid; ++uid){
 		 	if(uid_mems[uid]){
-		 		printf("mr_mem_info uid=%d, mem=%d \n", uid, uid_mems[uid]);
+		 		struct mr_mem_info* info = (struct mr_mem_info*)malloc(sizeof(struct mr_mem_info));
+		 		info->uid = uid;
+		 		info->mem = uid_mems[uid];
+		 		info->next = NULL;
+		 		if (head == NULL){
+		 			head = info;
+		 			tail = info;
+		 		}else{
+		 			tail->next = info;
+		 			tail = info;
+		 		}
 		 	}
-		 }
+		}
+		return head;
 	}
-	printf("mr_mem_info used_memory=%ldkb, memory_block=%ld \n", _used_memory>>10, _memory_block);
+	// printf("mr_mem_info used_memory=%ldkb, memory_block=%ld \n", _used_memory>>10, _memory_block);
+}
+
+size_t mr_get_used_memory(void){
+	return _used_memory;
+}
+
+size_t mr_get_memory_block(void){
+	return _memory_block;
 }
 
 void mr_mem_detect(uint32_t max_uid){
